ae2a65e39b8f8f3ab4fd8ef85cd3626249ff2d6a
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-01-01 18:28:00 +0100

    First commit

---
+++ AiSD-Projekt-3/AiSD-Projekt-3/AiSD-Projekt-3.cpp
@@ -0,0 +1,27 @@
+﻿// AiSD-Projekt-3.cpp
+/*
+# AiSD - Projekt-3
+Zadanie 6. Napisz program, który dla zadanego grafu skierowanego reprezentowanego przy pomocy listy krawędzi zapisanych przy pomocy dwóch tablic wyznaczy i wypisze następujące informacje :
+1) wszystkich sąsiadów dla każdego wierzchołka grafu(sąsiad wierzchołka wi, to ten wierzchołek do którego prowadzi krawędź z wi)
+2) wszystkie wierzchołki, które są sąsiadami każdego wierzchołka
+3) stopnie wychodzące wszystkich wierzchołków
+4) stopnie wchodzące wszystkich wierzchołków
+5) wszystkie wierzchołki izolowane
+6) wszystkie pętle
+7) wszystkie krawędzie dwukierunkowe
+Każdy z powyższych podpunktów powinien być realizowany jako oddzielna funkcja.
+W funkcji main() należy przedstawić działanie napisanej przez siebie biblioteki na reprezentatywnym przykładzie.
+Kod powinien być opatrzony stosownymi komentarzami.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    system("pause");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # AiSD-Projekt-3
+Zadanie 6. Napisz program, który dla zadanego grafu skierowanego reprezentowanego przy pomocy listy krawędzi zapisanych przy pomocy dwóch tablic wyznaczy i wypisze następujące informacje:
+
+1) wszystkich sąsiadów dla każdego wierzchołka grafu (sąsiad wierzchołka w, to ten wierzchołek do którego prowadzi krawędź z w₁)
+
+2) wszystkie wierzchołki, które są sąsiadami każdego wierzchołka
+
+3) stopnie wychodzące wszystkich wierzchołków
+
+4) stopnie wchodzące wszystkich wierzchołków
+
+5) wszystkie wierzchołki izolowane
+
+6) wszystkie pętle
+
+7) wszystkie krawędzie dwukierunkowe
+
+Każdy z powyższych podpunktów powinien być realizowany jako oddzielna funkcja.
+W funkcji main() należy przedstawić działanie napisanej przez siebie biblioteki na reprezentatywnym przykładzie.
+Kod powinien być opatrzony stosownymi komentarzami.
+
