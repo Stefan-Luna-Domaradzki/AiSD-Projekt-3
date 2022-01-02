4577a950bd7abd0983b7c6b41d1136a40b0d09fa
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-01-02 15:07:54 +0100

    added creating and printing adjacency matrix

--- AiSD-Projekt-3/AiSD-Projekt-3/AiSD-Projekt-3.cpp
+++ AiSD-Projekt-3/AiSD-Projekt-3/AiSD-Projekt-3.cpp
@@ -1,7 +1,8 @@
 ﻿// AiSD-Projekt-3.cpp
 /*
 # AiSD - Projekt-3
-Zadanie 6. Napisz program, który dla zadanego grafu skierowanego reprezentowanego przy pomocy listy krawędzi zapisanych przy pomocy dwóch tablic wyznaczy i wypisze następujące informacje :
+Zadanie 6. Napisz program, który dla zadanego grafu skierowanego reprezentowanego przy pomocy 
+listy krawędzi zapisanych przy pomocy dwóch tablic wyznaczy i wypisze następujące informacje :
 1) wszystkich sąsiadów dla każdego wierzchołka grafu(sąsiad wierzchołka wi, to ten wierzchołek do którego prowadzi krawędź z wi)
 2) wszystkie wierzchołki, które są sąsiadami każdego wierzchołka
 3) stopnie wychodzące wszystkich wierzchołków
@@ -15,13 +16,130 @@ Kod powinien być opatrzony stosownymi komentarzami.
 */
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <list>
 
 using namespace std;
 
+void edges_from_file(int edge_begin, int edge_end);
+void adding_edges(int edge_begin[], int edge_end[], int nodes, int size);
+
+//macierz w komórkach n będzie przechowywała węzły o wartości n+1
+void fill_adjacency_matrix(int edge_begin[], int edge_end[], int **adjacency_matrix, int nodes, int size);
+void print_adj_matrix(int** adjacency_matrix, int nodes);
+
+
 int main()
 {
-    cout << "Hello World!\n";
+    //std::list<int> krawedzie[10];
+
+    //krawedzie->push_back(1);
+
+    int nodes; 
+    int edges;
+
+
+    int* edge_begin;
+    int* edge_end;
+    
+    
+
+    cout << "\n Podaj ilosc wezlow: ";    cin >> nodes;
+    cout << "\n Podaj ilosc krawedzi : "; cin >> edges;
+
+
+    edge_begin = new int[edges];
+    edge_end   = new int[edges];
+    
+    int** adjacency_matrix = new int* [nodes];
+
+    for (int i = 0; i < nodes; ++i)
+        adjacency_matrix[i] = new int[nodes];
+
+    adding_edges(edge_begin, edge_end, nodes, edges);
+
+
+    fill_adjacency_matrix(edge_begin, edge_end, adjacency_matrix, nodes, edges);
+    print_adj_matrix(adjacency_matrix, nodes);
+
 
     system("pause");
     return 0;
+}
+
+void edges_from_file(int edge_begin, int edge_end)
+{
+    {
+        /* ifstream edges;
+           edges.open("edges.txt", ios::in);
+           string line;
+           while (!edges.eof()){cout << "xD";}
+           edges.close();*/
+    }
+}
+
+void adding_edges(int edge_begin[], int edge_end[], int nodes, int size)
+{
+    int add_edge;
+    bool flag;
+
+
+    cout << "\n dostepne wezly : "; for (int i = 0; i < nodes; i++) cout << " " << i + 1;
+    cout << "\n";
+
+
+
+    for (int i = 0; i < size; i++)
+    {
+        flag = false;
+
+        cout << "\n Podaj wezel poczatku i konca (2 liczby oddzielone spaja) krawedzi  '" << i + 1 << "': "; 
+        
+        cin >> add_edge;
+        if (add_edge <1 || add_edge>nodes)
+        {
+            flag = true;
+            cout << "\n brak wezla: " << add_edge;
+        }
+        edge_begin[i] = add_edge;
+        
+        
+        cin >> add_edge; edge_end[i]   = add_edge;
+        if (add_edge <1 || add_edge>nodes)
+        {
+            flag = true;
+            cout << "\n brak wezla: " << add_edge;
+        }
+
+        if (flag) i--;
+    }
+
+
+
+    for (int i = 0; i < size; i++)
+    {cout << edge_begin[i] << "->" << edge_end[i] <<"\n"; }
+}
+
+void fill_adjacency_matrix(int edge_begin[], int edge_end[], int** adjacency_matrix, int nodes, int size)
+{
+    for (int i = 0; i < nodes; i++)
+        for (int j = 0; j < nodes; j++)
+            adjacency_matrix[i][j] = 0;
+
+    for (int i = 0; i < size; i++)
+        adjacency_matrix[edge_begin[i] - 1][edge_end[i] - 1] = 1;
+}
+
+void print_adj_matrix(int** adjacency_matrix, int nodes)
+{
+    cout << endl << setw(4);
+
+    for (int i = 0; i < nodes; i++)
+    {
+        for (int j = 0; j < nodes; j++)
+            cout << adjacency_matrix[i][j] << setw(4);
+        cout << endl;
+    }
 }
