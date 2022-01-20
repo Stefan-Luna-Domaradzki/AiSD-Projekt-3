7311b0c63df7edf9e37bc3d7a1268203097167af
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-01-20 23:59:34 +0100

    Poprawione do list

--- AiSD-Projekt-3/AiSD-Projekt-3/AiSD-Projekt-3.cpp
+++ AiSD-Projekt-3/AiSD-Projekt-3/AiSD-Projekt-3.cpp
@@ -27,17 +27,20 @@ void edges_from_file(int edge_begin, int edge_end);
 void adding_edges(int edge_begin[], int edge_end[], int nodes, int size);
 
 //macierz w komórkach n będzie przechowywała węzły o wartości n+1
-void fill_adjacency_matrix(int edge_begin[], int edge_end[], int **adjacency_matrix, int nodes, int size);
-void print_adj_matrix(int** adjacency_matrix, int nodes);
+//void fill_adjacency_matrix(int edge_begin[], int edge_end[], int **adjacency_matrix, int nodes, int size);
+//void print_adj_matrix(int** adjacency_matrix, int nodes);
 
 
-void all_neighbours(int** adjacency_matrix, int nodes);
-void everyones_neighbour(int** adjacency_matrix, int nodes);
+void all_neighbours(int edge_begin[], int edge_end[], int nodes, int size);
+void everyones_neighbour(int edge_begin[], int edge_end[], int nodes, int size);
 
-void find_degree_of_edges(int** adjacency_matrix, int nodes);
+void find_degree_of_edges(int edge_begin[], int edge_end[], int nodes, int size);
 
-void find_loops(int** adjacency_matrix, int nodes);
-void find_2way_edges(int** adjacency_matrix, int nodes);
+void find_loops(int edge_begin[], int edge_end[], int nodes, int size);
+void find_2way_edges(int edge_begin[], int edge_end[], int nodes, int size);
+
+void find_isolated_node(int edge_begin[], int edge_end[], int nodes, int size);
+
 
 int main()
 {
@@ -61,27 +64,20 @@ int main()
     edge_begin = new int[edges];
     edge_end   = new int[edges];
     
-    int** adjacency_matrix = new int* [nodes];
-
-    for (int i = 0; i < nodes; ++i)
-        adjacency_matrix[i] = new int[nodes];
 
     adding_edges(edge_begin, edge_end, nodes, edges);
 
 
-    fill_adjacency_matrix(edge_begin, edge_end, adjacency_matrix, nodes, edges);
-    print_adj_matrix(adjacency_matrix, nodes);
-
-    all_neighbours(adjacency_matrix, nodes);
+    all_neighbours(edge_begin, edge_end, nodes, edges);
 
     cout << endl;
 
-    everyones_neighbour(adjacency_matrix, nodes);
+    everyones_neighbour(edge_begin, edge_end, nodes, edges);
 
-    find_degree_of_edges(adjacency_matrix, nodes);
+    find_degree_of_edges(edge_begin, edge_end, nodes, edges);
 
-    find_loops(adjacency_matrix, nodes);
-    find_2way_edges(adjacency_matrix, nodes);
+    find_loops(edge_begin, edge_end, nodes, edges);
+    find_2way_edges(edge_begin, edge_end, nodes, edges);
 
 
     system("pause");
@@ -141,82 +137,64 @@ void adding_edges(int edge_begin[], int edge_end[], int nodes, int size)
     {cout << edge_begin[i] << "->" << edge_end[i] <<"\n"; }
 }
 
-void fill_adjacency_matrix(int edge_begin[], int edge_end[], int** adjacency_matrix, int nodes, int size)
+void all_neighbours(int edge_begin[], int edge_end[], int nodes, int size)
 {
-    for (int i = 0; i < nodes; i++)
-        for (int j = 0; j < nodes; j++)
-            adjacency_matrix[i][j] = 0;
 
-    for (int i = 0; i < size; i++)
-        adjacency_matrix[edge_begin[i] - 1][edge_end[i] - 1] = 1;
-}
 
-void print_adj_matrix(int** adjacency_matrix, int nodes)
-{
-    cout << endl << "Macierz incydencji" << endl << setw(4);
-
-    for (int i = 0; i < nodes; i++)
+    for (int i = 0; i < nodes; i++) 
     {
-        for (int j = 0; j < nodes; j++)
-            cout << adjacency_matrix[i][j] << setw(4);
-        cout << endl;
-    }
-}
-
-void all_neighbours(int** adjacency_matrix, int nodes)
-{
-    bool izolowany;
-
-    for (int i = 0; i < nodes; i++)
-    {
-        izolowany = true;
-
         cout << "\n Wszyscy sasiedzi wierzcholka " << i + 1 << ": ";
 
-        for (int j = 0; j < nodes; j++)
+        for (int j = 0; j < size; j++)
         {
-            if (adjacency_matrix[j][i] == 1)//np [2][1] to znaczy że z 1 do 2 jest '->' czyli 1 jest sąsiadem 2
+            if (edge_end[j] == (i + 1))//np [2][1] to znaczy że z 1 do 2 jest '->' czyli 1 jest sąsiadem 2
             {
-                cout << j + 1 << ", ";
-                izolowany = false;
+                cout << edge_begin[j] << ", ";
             }
         }
 
-        if (izolowany) cout << " brak krawedzi wchodzacych. Wierzcholek izolowany";
     }
 }
 
-void everyones_neighbour(int** adjacency_matrix, int nodes)
+void everyones_neighbour(int edge_begin[], int edge_end[], int nodes, int size)
 {
-    //można zmniejszyć ilość porównać dając break
     bool is_neighbour = true;
-    bool exists = false;
 
-    cout << "\n Wierzcholki sasiadujace ze wszystkimi innymi wierzcholkami: ";
+    int* unikalne = new int[nodes];
 
     for (int i = 0; i < nodes; i++)
     {
-        exists = is_neighbour;
+        unikalne[i] = 0;
+    }
+
+    cout << "\n Wierzcholki sasiadujace ze wszystkimi innymi wierzcholkami: ";
+
+    for (int i = 0; i < size; i++)
+    {
         is_neighbour = true;
+
+        for (int j = 0; j < size; j++)
+        {
+            if (edge_begin[j] == (i + 1))
+            {
+                unikalne[edge_end[j] - 1]=1;
+            }
+        }
 
         for (int j = 0; j < nodes; j++)
         {
-            if (adjacency_matrix[i][j] == 0 && j != i) //wtedy węzeł i nie jest sąsiadem wszystkich
-            {
-            is_neighbour = false;
-            break;           
-            }
+            if (unikalne[j]!=1 && j != i)
+                is_neighbour = false;
         }
 
         if (is_neighbour) cout << i + 1 << ", ";
     }
 
-
-    if (!exists) cout << "brak. \n";
+    delete[] unikalne;
 }
 
 
-void find_degree_of_edges(int** adjacency_matrix, int nodes)
+void find_degree_of_edges(int edge_begin[], int edge_end[], int nodes, int size)
 {
     int outgoing_edges = 0;
     int incoming_edges = 0;
@@ -228,14 +206,14 @@ void find_degree_of_edges(int** adjacency_matrix, int nodes)
         incoming_edges = 0;
 
 
-        for (int j = 0; j < nodes; j++)
+        for (int j = 0; j < size; j++)
         {
-            if (adjacency_matrix[i][j] == 1)
+            if (edge_begin[j]==(i+1))
             {
                 outgoing_edges++;
             }
 
-            if (adjacency_matrix[j][i] == 1)
+            if (edge_end[j] == (i+1))
             {
                 incoming_edges++;
             }
@@ -247,23 +225,49 @@ void find_degree_of_edges(int** adjacency_matrix, int nodes)
     }
 }
 
-void find_loops(int** adjacency_matrix, int nodes)
+
+void find_loops(int edge_begin[], int edge_end[], int nodes, int size)
 {
     cout << "\n Wierzcholki posiadajace petle: ";
 
-    for (int i = 0; i < nodes; i++)
-        if (adjacency_matrix[i][i]) cout << i + 1 << ", ";
+    for (int i = 0; i < size; i++)
+        if (edge_begin[i]==edge_end[i]) cout << i+1 << ", ";
 }
 
-void find_2way_edges(int** adjacency_matrix, int nodes)
+void find_2way_edges(int edge_begin[], int edge_end[], int nodes, int size)
 {
+    bool is_2way_edge = false;
+
     for (int i = 0; i < nodes; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < size; j++)
         {
-            if (adjacency_matrix[i][j] && adjacency_matrix[j][i])
-                cout << "\n Znaleziono dwukierunkowa krawedz miedzy: " << i + 1 << " i " << j + 1 << ".";
+            if (edge_begin[j]==i+1)
+            {
+                for (int k=0; k<size; k++)
+                {
+                    if (edge_begin[k] == edge_end[j])
+                        if (edge_end[k] == i + 1)
+                            cout << "\n Znaleziono 2kierunkowa miedzy: " << i + 1 << " i " << edge_begin[k];
+                }
+            }
+        }
+    }
+}
+
+void find_isolated_node(int edge_begin[], int edge_end[], int nodes, int size)
+{
+    bool is_isolated = true;
+    for (int i = 0; i < nodes; i++)
+    {
+        is_isolated = true;
+
+        for (int j = 0; j < size; j++)
+        {
+            if (edge_begin[j] == (i + 1) || edge_end[j] == (i + 1))
+                is_isolated=false;
         }
 
+        if (is_isolated) cout << "\n Wezel " << i + 1 << "jest izolowany";
     }
 }
